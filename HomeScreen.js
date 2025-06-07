b7bb59491742dd1be5bcc8a33727028ee9751ce7
Leslie1234 <nokaleslie@gmail.com>
2025-06-07 06:59:47 +0100

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Expense Tracker!</Text>
+      <Button title="Logout" onPress={() => navigation.replace('Login')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 22, marginBottom: 20 }
+});
